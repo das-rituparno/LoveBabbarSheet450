357ec6acf7db9d888163abce4331d5baa3e8ba4c
Rituparno Das <95102828+Almost-Developer@users.noreply.github.com>
2023-10-04 01:08:37 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum number of jumps/minimum-number-of-jumps.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+class GFG
+ {
+	public static void main (String[] args) throws IOException
+	 {
+	 
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        int t = Integer.parseInt(br.readLine()); 
+
+        while(t-- > 0){
+            int size = Integer.parseInt(br.readLine());
+            String[] arrStr = ((String)br.readLine()).split("\\s+");
+            int[] arr= new int[size];
+            for(int i = 0;i<size;i++){
+                arr[i] = Integer.parseInt(arrStr[i]);
+            }
+            System.out.println(new Solution().minJumps(arr));
+        }
+	 }
+	 
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    static int minJumps(int[] arr){
+        // your code here
+        if(arr.length <= 1) return 0;
+        if(arr[0] == 0) return -1;
+        
+        int step = arr[0];
+        int reach = arr[0];
+        int jump = 1;
+        
+        for(int i=1; i<arr.length; i++) {
+            if(i == arr.length-1) return jump;
+            reach = Math.max(reach, i+arr[i]);
+            step--;
+            if(step == 0) {
+                jump++;
+                if(i >= reach) return -1;
+                step = reach - i;
+            }
+        }
+        return -1;
+    }
+}
