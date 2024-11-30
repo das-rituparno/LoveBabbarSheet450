f9d3581f39d74c520f89764472f929adb211d5db
Rituparno Das <95102828+das-rituparno@users.noreply.github.com>
2024-11-30 13:21:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.java
+++ Difficulty: Easy/Anagram/anagram.java
@@ -33,18 +33,35 @@ class Solution {
     public static boolean areAnagrams(String s1, String s2) {
 
         // Your code here
-        return sorting(s1, s2);
-    }
-    
-    public static boolean sorting(String s1, String s2) {
-        if(s1.length() != s2.length()) return false;
+        HashMap<Character, Integer> map = new HashMap<>();
         
-        char[] arr1 = s1.toCharArray();
-        char[] arr2 = s2.toCharArray();
+        for(int i=0; i<s1.length(); i++) {
+            char ch = s1.charAt(i);
+            
+            if(map.containsKey(ch)) {
+                map.put(ch, map.get(ch) + 1);
+            }
+            else {
+                map.put(ch, 1);
+            }        
+        }
         
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
+        for(int i=0; i<s2.length(); i++) {
+            char ch = s2.charAt(i);
+            
+            if(map.containsKey(ch)) {
+                map.put(ch, map.get(ch) - 1);
+            }
+            
+            else {
+                map.put(ch, 1);
+            }
+            
+            if(map.get(ch) == 0) {
+                map.remove(ch);
+            }
+        }
         
-        return Arrays.equals(arr2, arr1);
+        return map.isEmpty();
     }
 }
