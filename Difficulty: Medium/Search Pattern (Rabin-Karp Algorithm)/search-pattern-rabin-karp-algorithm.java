57fe8eba355080f4b495187bb848b6f56ff3d145
Rituparno Das <95102828+das-rituparno@users.noreply.github.com>
2025-06-06 00:22:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.java
@@ -0,0 +1,57 @@
+class Solution {
+    ArrayList<Integer> search(String pat, String txt) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int m = pat.length();
+        int n = txt.length();
+
+        // Step 1: Build LPS array
+        int[] lps = computeLPS(pat);
+
+        int i = 0; // index for txt
+        int j = 0; // index for pat
+
+        while (i < n) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                i++;
+                j++;
+            }
+
+            if (j == m) {
+                result.add(i - j + 1); // 1-based index
+                j = lps[j - 1];
+            } else if (i < n && pat.charAt(j) != txt.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private int[] computeLPS(String pat) {
+        int m = pat.length();
+        int[] lps = new int[m];
+        int len = 0; // length of the previous longest prefix suffix
+        int i = 1;
+
+        while (i < m) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return lps;
+    }
+}
