56538d93c3b4979424fdfbad8458379717d24e68
Rituparno Das <95102828+das-rituparno@users.noreply.github.com>
2025-06-24 20:15:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find nth root of m/find-nth-root-of-m.java
@@ -0,0 +1,22 @@
+// User function Template for Java
+
+class Solution {
+    public int nthRoot(int n, int m) {
+        // code here
+        int low = 1;
+        int high = m;
+
+        while(low <= high) {
+            int mid = (low+high)/2;
+
+            if(Math.pow(mid, n) == m) return mid;
+            else if (Math.pow(mid, n) > m) {
+                high = mid-1;
+            }
+            else {
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+}
