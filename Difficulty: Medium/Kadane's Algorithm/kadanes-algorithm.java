eb57f79d2a103d1cd4899468f30fab5a9c3ff184
Rituparno Das <95102828+das-rituparno@users.noreply.github.com>
2026-01-05 22:05:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -1,58 +1,12 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
-        while (t-- > 0) {
-
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            Solution obj = new Solution();
-
-            // calling maxSubarraySum() function
-            System.out.println(obj.maxSubarraySum(arr));
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
 class Solution {
-
-    // arr: input array
-    // Function to find the sum of contiguous subarray with maximum sum.
     int maxSubarraySum(int[] arr) {
-
-        // Your code here
-        int temp = 0;
-        int potential = Integer.MIN_VALUE;
-        
-        for(int i=0; i<arr.length; i++) {
-            temp += arr[i];
-            potential = Math.max(temp, potential);
-            if(temp < 0) temp = 0;
+        // Code here
+        int sum = 0, potential = Integer.MIN_VALUE;
+        for(int item : arr) {
+            sum += item;
+            potential = Math.max(potential, sum);
+            if(sum < 0) sum = 0;
         }
-        
         return potential;
     }
 }
