c6cfdf06582764c4b38d7674a48b6d2dd7d5374d
Rituparno Das <95102828+das-rituparno@users.noreply.github.com>
2026-01-05 20:55:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Min and Max in Array/min-and-max-in-array.java
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.java
@@ -1,101 +1,14 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-
-// } Driver Code Ends
-// User function Template for Java
-// User function Template for Java
-
-/*
-class Pair<K, V> {
-    private final K key;
-    private final V value;
-
-    public Pair(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public K getKey() {
-        return key;
-    }
-
-    public V getValue() {
-        return value;
-    }
-}
-
-Java users need to return result in Pair class
-For Example -> return new Pair(minimum,maximum)
-*/
-
 class Solution {
-    public Pair<Long, Long> getMinMax(int[] arr) {
-        // Code Here
-        
-        Long small = Long.MAX_VALUE;
-        Long big = Long.MIN_VALUE;
-        
-        for(int i=0; i<arr.length; i++) {
-            small = Math.min(small, arr[i]);
-            big = Math.max(big, arr[i]);
+    public ArrayList<Integer> getMinMax(int[] arr) {
+        // code Here
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int minimum=Integer.MAX_VALUE, maximum=Integer.MIN_VALUE;
+        for(int item : arr) {
+            minimum = Math.min(minimum, item);
+            maximum = Math.max(maximum, item);
         }
-        
-        Pair<Long, Long> pair = new Pair<>(small, big);
-        return pair;
-        
+        list.add(minimum);
+        list.add(maximum);
+        return list;
     }
 }
-
-
-//{ Driver Code Starts.
-
-class Pair<K, V> {
-    private final K key;
-    private final V value;
-
-    public Pair(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public K getKey() { return key; }
-
-    public V getValue() { return value; }
-}
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            Solution ob = new Solution();
-            Pair<Long, Long> pp = ob.getMinMax(arr);
-            System.out.println(pp.getKey() + " " + pp.getValue());
-        
-System.out.println("~");
-}
-    }
-}
-
-// } Driver Code Ends
