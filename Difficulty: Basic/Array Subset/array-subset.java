291c14778e7c85a5eced758a6e4e5703edb787d8
Rituparno Das <95102828+das-rituparno@users.noreply.github.com>
2026-01-06 18:53:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Subset/array-subset.java
+++ Difficulty: Basic/Array Subset/array-subset.java
@@ -1,68 +1,19 @@
-//{ Driver Code Starts
-//Initial Template for Java
 
-//Initial Template for Java
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-class GFG {
-	public static void main(String[] args) throws IOException
-	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-        while(t-->0)
-        {
-            StringTokenizer stt = new StringTokenizer(br.readLine());
-            
-            long n = Long.parseLong(stt.nextToken());
-            long m = Long.parseLong(stt.nextToken());
-            long a1[] = new long[(int)(n)];
-            long a2[] = new long[(int)(m)];
-            
-            
-            String inputLine[] = br.readLine().trim().split(" ");
-            for (int i = 0; i < n; i++) {
-                a1[i] = Long.parseLong(inputLine[i]);
-            }
-            String inputLine1[] = br.readLine().trim().split(" ");
-            for (int i = 0; i < m; i++) {
-                a2[i] = Long.parseLong(inputLine1[i]);
-            }
-            
-            
-            Compute obj = new Compute();
-            System.out.println(obj.isSubset( a1, a2, n, m));
-            
-        }
-	}
-}
-
-// } Driver Code Ends
-
-
-//User function Template for Java
-
-
-class Compute {
-    public String isSubset( long a1[], long a2[], long n, long m) {
-        // code here
+class Solution {
+    public boolean isSubset(int a[], int b[]) {
+        // Your code here
+        int i=0, j=0;
         
-        Map<Long, Integer> hm = new HashMap<>();
-        
-        for(int i=0; i<n; i++) {
-            hm.put(a1[i], hm.getOrDefault(a1[i], 0) +1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int it : a) {
+            map.put(it, map.getOrDefault(it, 0)+1);
         }
         
-        for(int i=0; i<m; i++) {
-            long num = a2[i];
-            if(hm.getOrDefault(num, 0) > 0) {
-                hm.put(num, hm.get(num) - 1);
-            }
-            else return "No";
+        for(int it : b) {
+            if(!map.containsKey(it) || map.get(it) == 0) return false;
+            map.put(it, map.get(it)-1);
         }
         
-        return "Yes";
+        return true;
     }
 }
