fe23a9ec866e8458353a2e3a1732f7fdf23f124d
Rituparno Das <95102828+das-rituparno@users.noreply.github.com>
2026-02-09 19:15:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.java
+++ Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.java
@@ -1,41 +1,19 @@
-//{ Driver Code Starts
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int t = Integer.parseInt(scanner.nextLine());
-
-        Solution ob = new Solution(); // Instantiate the Solution object once
-
-        while (t-- > 0) {
-            String line = scanner.nextLine();
-            String[] elements = line.split(" ");
-            int[] arr = new int[elements.length]; // Changed to int[]
-
-            for (int i = 0; i < elements.length; i++) {
-                arr[i] = Integer.parseInt(elements[i]);
-            }
-
-            System.out.println(ob.findMin(arr));
-            System.out.println("~");
-        }
-        scanner.close();
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     public int findMin(int[] arr) {
-        // complete the function here
-        int res = Integer.MAX_VALUE;
-        for(int item : arr) {
-            res = Math.min(res, item);
+        // code here
+        int start = 0;
+        int end = arr.length-1;
+        int potential = 0;
+        
+        while(start < end) {
+            int mid = (start+end)/2;
+            
+            if(arr[mid] > arr[end]) {
+                start = mid+1;
+            }
+            else end = mid;
         }
-        return res;
+        
+        return arr[start];
     }
 }
