9897c4d06346ea681b5be2f5e4ff5dfab43fcfde
Rituparno Das <95102828+das-rituparno@users.noreply.github.com>
2026-02-10 10:18:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.java
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -1,48 +1,24 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            System.out.println(new Solution().majorityElement(arr));
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    static int majorityElement(int arr[]) {
-        // your code here
+    int majorityElement(int arr[]) {
+        // code here
         int candidate = -1;
         int count = 0;
-        
-        Arrays.sort(arr);
-        for(int i=0; i<arr.length; i++) {
-            if(count == 0) {
-                candidate = arr[i];
-                count = 1;
-            }
-            else if(arr[i] == candidate) count++;
-            else count--;
-        }
-        
-        count = 0;
-        for(int item : arr) {
-            if(item == candidate) {
+        int max_count = 0;
+
+        for (int item : arr) {
+            if (count == 0) {
+                candidate = item;
                 count++;
             }
+            else if(candidate == item) {
+                count++;
+            }
+            else count--;
+        }
+
+        count = 0;
+        for (int item : arr) {
+            if (candidate == item) count++;
         }
         
         return count > arr.length/2 ? candidate : -1;
