a239241dec7540f6993ba0f03d707501000c69bd
Rituparno Das <95102828+das-rituparno@users.noreply.github.com>
2026-02-26 13:20:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Print Anagrams Together/print-anagrams-together.java
+++ Difficulty: Medium/Print Anagrams Together/print-anagrams-together.java
@@ -1,53 +1,19 @@
-//{ Driver Code Starts
-import java.util.*;
-
-
-// } Driver Code Ends
 class Solution {
     public ArrayList<ArrayList<String>> anagrams(String[] arr) {
         // code here
-        HashMap<String, List<String>> map = new HashMap<>();
-        
-        for(String item : arr) {
+        Map<String, ArrayList<String>> map = new HashMap<>();
+
+        for (String item : arr) {
             char[] temp = item.toCharArray();
             Arrays.sort(temp);
-            String sorted = new String(temp);
-            
-            if(!map.containsKey(sorted)) {
-                map.put(sorted, new ArrayList());
+            String sortedKey = new String(temp);
+
+            if (!map.containsKey(sortedKey)) {
+                map.put(sortedKey, new ArrayList<>());
             }
-            map.get(sorted).add(item);
+            map.get(sortedKey).add(item);
         }
-        
-        return new ArrayList(map.values());
+
+        return new ArrayList<>(map.values());
     }
 }
-
-
-//{ Driver Code Starts.
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        sc.nextLine(); // Ignore the newline after the test case input
-        while (t-- > 0) {
-            String inputLine = sc.nextLine();
-            String[] arr = inputLine.split(" ");
-
-            Solution ob = new Solution();
-            ArrayList<ArrayList<String>> result = ob.anagrams(arr);
-            result.sort(Comparator.comparing(a -> a.get(0)));
-            for (ArrayList<String> group : result) {
-                for (String word : group) {
-                    System.out.print(word + " ");
-                }
-                System.out.println();
-            }
-            System.out.println("~");
-        }
-        sc.close();
-    }
-}
-
-// } Driver Code Ends
