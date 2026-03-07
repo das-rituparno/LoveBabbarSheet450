8511eed7a3e03192a4a0ac006219a016b13533a0
Rituparno Das <95102828+das-rituparno@users.noreply.github.com>
2026-03-07 12:47:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Without Extra Space/merge-without-extra-space.java
+++ Difficulty: Medium/Merge Without Extra Space/merge-without-extra-space.java
@@ -1,25 +1,21 @@
 class Solution {
     public void mergeArrays(int a[], int b[]) {
         // code here
-        int index = 0;
-        while (index < a.length) {
-            if (a[index] > b[0]) {
-                int temp = a[index];
-                a[index] = b[0];
+        int idx = 0;
+        while(idx < a.length) {
+            if(a[idx] > b[0]) {
+                int temp = a[idx];
+                a[idx] = b[0];
                 b[0] = temp;
-
+                
                 int k;
                 int first = b[0];
-                for (k=1; k<b.length && first > b[k]; k++) {
+                for(k=1; k<b.length && first > b[k]; k++) {
                     b[k-1] = b[k];
                 }
                 b[k-1] = first;
             }
-            index++;
+            idx++;
         }
     }
 }
-/*
-Time Complexity - O(N)
-Space Complexity - O(1)
-*/
