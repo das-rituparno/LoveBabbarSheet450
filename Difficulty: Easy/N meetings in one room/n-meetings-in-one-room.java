f2f56702a9d0894869be72bb1eb2777b2f8dc24c
Rituparno Das <95102828+das-rituparno@users.noreply.github.com>
2026-03-19 01:07:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.java
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.java
@@ -1,28 +1,20 @@
 class Solution {
-    // Function to find the maximum number of meetings that can
-    // be performed in a meeting room.
     public int maxMeetings(int start[], int end[]) {
         // add your code here
-         List<Meeting> meetings = new ArrayList<>();
-        for (int i=0; i<start.length; i++) {
+        List<Meeting> meetings = new ArrayList<>();
+        for(int i=0; i<start.length; i++) {
             meetings.add(new Meeting(start[i], end[i]));
         }
         meetings.sort(Comparator.comparingInt(m -> m.end));
-        // System.out.println("Meetings after sorting by end time:");
-        // for (Meeting m : meetings) {
-        //     System.out.println("(" + m.start + ", " + m.end + ")");
-        // }
-
         int count = 0;
-        int lastEndTime = -1;
-
-        for (Meeting item : meetings) {
-            if (item.start > lastEndTime) {
+        int last_end = -1;
+        
+        for(Meeting item : meetings) {
+            if(item.start > last_end) {
                 count++;
-                lastEndTime = item.end;
+                last_end = item.end;
             }
         }
-
         return count;
     }
 }
