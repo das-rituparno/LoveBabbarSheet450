22af3d19e637e688f89974b535c3eb540d3b56d1
Rituparno Das <95102828+das-rituparno@users.noreply.github.com>
2026-03-23 00:03:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.java
+++ Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.java
@@ -2,18 +2,21 @@ class Solution {
     int maxLength(int arr[]) {
         // code here
         HashMap<Integer, Integer> map = new HashMap<>();
-        int sum = 0, max_sum = 0;
+        map.put(0, -1);
+        int sum = 0;
+        int ans = 0;
         
         for(int i=0; i<arr.length; i++) {
             sum += arr[i];
             
-            if(sum == 0) max_sum = i+1;
-            
             if(map.containsKey(sum)) {
-                max_sum = Math.max(max_sum, i-map.get(sum));
+                ans = Math.max(ans, i - map.get(sum));
             }
-            else map.put(sum, i);
+            
+            if(!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
         }
-        return max_sum;
+        return ans;
     }
 }
