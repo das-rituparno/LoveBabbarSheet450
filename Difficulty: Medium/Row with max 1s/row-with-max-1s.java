af5ce06b5fc87e4d12fd1b93808196e6a23c79a4
Rituparno Das <95102828+das-rituparno@users.noreply.github.com>
2026-03-30 10:37:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.java
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.java
@@ -9,8 +9,8 @@ class Solution {
         
         while(row < arr.length && col >= 0) {
             if(arr[row][col] == 1) {
-                col--;
                 max_ones = row;
+                col--;
             }
             else row++;
         }
