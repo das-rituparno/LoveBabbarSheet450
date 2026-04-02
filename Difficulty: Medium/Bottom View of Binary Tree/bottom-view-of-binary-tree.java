0a5700433d55020fa6bb41ba81459215ad7a27fd
Rituparno Das <95102828+das-rituparno@users.noreply.github.com>
2026-04-02 15:21:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.java
+++ Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.java
@@ -1,44 +1,25 @@
-/*
-class Node
-{
-    int data; //data of the node
-    int hd; //horizontal distance of the node
-    Node left, right; //left and right references
-
-    // Constructor of tree node
-    public Node(int key)
-    {
-        data = key;
-        hd = Integer.MAX_VALUE;
-        left = right = null;
-    }
-}
-*/
-
 class Solution {
     public ArrayList<Integer> bottomView(Node root) {
-        // Code here
-        Map<Integer,Integer> map = new TreeMap<>();
-        ArrayList<Integer> list = new ArrayList<>();
-        if(root == null) return list;
-
+        // code here
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(root,0));
-
+        ArrayList<Integer> list = new ArrayList<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        if(root == null) return list;
+        q.add(new Pair(root, 0));
+        
         while(!q.isEmpty()) {
             Pair first = q.poll();
-            Node temp = first.node;
-            int lev = first.level;
-
-            map.put(lev, temp.data);
-            if(temp.left != null) q.add(new Pair(temp.left, lev-1));
-            if(temp.right != null) q.add(new Pair(temp.right, lev+1));
+            
+            Node node = first.node;
+            int hd = first.level;
+            
+            map.put(hd, node.data);
+            if(node.left != null) q.add(new Pair(node.left, hd-1));
+            if(node.right != null) q.add(new Pair(node.right, hd+1));
         }
-
-        for (Map.Entry<Integer, Integer> tm : map.entrySet()) {
+        for(Map.Entry<Integer, Integer> tm : map.entrySet()) {
             list.add(tm.getValue());
         }
-
         return list;
     }
 }
